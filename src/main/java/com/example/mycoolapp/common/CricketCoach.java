abd8ca659db9e08e8f5a749ed3696dcae33c772c
Anthony <oduekene@gmail.com>
2024-05-16 15:45:51 -0300

    implementing primary components

--- src/main/java/com/example/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/example/mycoolapp/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package com.example.mycoolapp.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class CricketCoach implements Coach{
 
     @Override
--- src/main/java/com/example/mycoolapp/rest/DemoController.java
+++ src/main/java/com/example/mycoolapp/rest/DemoController.java
@@ -2,25 +2,19 @@ package com.example.mycoolapp.rest;
 
 import com.example.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    private Coach coach;
-
-//    @Autowired
-//    DemoController(Coach coach) {
-//        this.coach = coach;
-//    }
+    private final Coach coach;
 
     @Autowired
-    public void setCoach(@Qualifier("baseBallCoach") Coach coach) {
+    DemoController(Coach coach) {
         this.coach = coach;
     }
+
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
