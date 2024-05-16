7c7c081452576252b70b99ad917d89ca17565a88
Anthony <oduekene@gmail.com>
2024-05-16 16:17:17 -0300

    creating lazy for global use

--- src/main/java/com/example/mycoolapp/common/CricketCoach.java
+++ src/main/java/com/example/mycoolapp/common/CricketCoach.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class CricketCoach implements Coach{
 
     public CricketCoach() {
--- src/main/java/com/example/mycoolapp/rest/DemoController.java
+++ src/main/java/com/example/mycoolapp/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.mycoolapp.rest;
 
 import com.example.mycoolapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private final Coach coach;
 
     @Autowired
-    DemoController(Coach coach) {
+    DemoController(@Qualifier(value = "tennisCoach") Coach coach) {
         System.out.println("In DemoController constructor: " + getClass().getSimpleName());
         this.coach = coach;
     }
